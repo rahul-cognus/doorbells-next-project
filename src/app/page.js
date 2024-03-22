303173f408ad16652812123a2d625a7d41322945
Rahul Jangid <rahul.jangid.cognus@gmail.com>
2024-03-22 19:11:27 +0530

    Changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
 "use client"
-import React from 'react'
+import React ,{useEffect} from 'react'
 import Image from "next/image";
 import Header from "@/ui/layout/Header";
+
+// CSS Plugin
 import "@/public/vendor/bootstrap-icons/bootstrap-icons.css"
 import "@/public/vendor/font-awesome/css/all.min.css"
+import "@/public/vendor/tiny-slider/tiny-slider.css"
+import "@/public/vendor/plyr/plyr.css"
 
 export default function Home() {
-  React.useEffect(() => {
+  useEffect(() => {
     import("bootstrap/dist/js/bootstrap.js");
   }, []);
   return (
